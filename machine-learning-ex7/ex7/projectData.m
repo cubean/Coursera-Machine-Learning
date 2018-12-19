23266d0a412a8dc12aa0aad5032dc5e7cbb247aa
Cubean <cubean.liu@gmail.com>
2018-12-19 21:35:28 +1100

    finish all

--- machine-learning-ex7/ex7/projectData.m
+++ machine-learning-ex7/ex7/projectData.m
@@ -9,6 +9,8 @@ function Z = projectData(X, U, K)
 % You need to return the following variables correctly.
 Z = zeros(size(X, 1), K);
 
+U
+K
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the projection of the data using only the top K 
 %               eigenvectors in U (first K columns). 
@@ -18,7 +20,7 @@ Z = zeros(size(X, 1), K);
 %                    projection_k = x' * U(:, k);
 %
 
-
+Z = X * U(:, 1:K);
 
 
 % =============================================================
